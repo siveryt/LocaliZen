d7f625c49fcbc0fe8a7008022cd69f4cde423b41
siveryt <git@sivery.de>
2024-10-12 15:57:00 +0200

    feat: Interface for XCStrings file

---
+++ src/types/XCStrings.d.ts
@@ -0,0 +1,23 @@
+import LanguageCode from 'iso-639-1';
+
+// Define a type for translation states
+type TranslationState = 'translated' | 'new' | 'needs_review';
+
+interface TranslationCatalog {
+  sourceLanguage: LanguageCode;
+  strings: Record<string, TranslationString>;
+  version: string;
+}
+
+interface TranslationString {
+  shouldTranslate?: boolean;
+  localizations?: Record<LanguageCode, Localization>;
+  comment?: string;
+}
+
+interface Localization {
+  stringUnit: {
+    state: TranslationState;
+    value: string;
+  };
+}
